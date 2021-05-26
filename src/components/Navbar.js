a7ef00d41141a185e2caaa60f045cbf8422f9704
Beksultan <59771095+Beka23@users.noreply.github.com>
2021-05-26 14:06:53 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
-import React from "react"
-import { useHistory } from "react-router-dom"
+import React from "react";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 function Navbar({ currentUser, setCurrentUser }) {
 
