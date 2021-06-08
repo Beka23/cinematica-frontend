6a221f14ca05712b8829d146495c1b5d584bd479
Beksultan <59771095+Beka23@users.noreply.github.com>
2021-06-08 12:21:29 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./Navbar"
+import Navbar from "./Navbar";
 import ReactPlayer from 'react-player'
 
 function Header({ currentUser, setCurrentUser}) {
