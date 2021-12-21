74c883236f312e4e705d08e82b11a43cf3e50773
Beksultan <59771095+Beka23@users.noreply.github.com>
2021-12-21 15:18:37 -0500

    Update MovieDetails.js

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react"
+import { React, useEffect, useState } from "react";
 import ReactPlayer from 'react-player'
 import { useParams } from "react-router-dom"
 import LikesDislikes from "./LikesDislikes"
