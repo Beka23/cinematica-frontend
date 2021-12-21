0ca4e8b708bf46b52f2913d3596d17d61ab4367d
Beksultan <59771095+Beka23@users.noreply.github.com>
2021-12-21 15:18:15 -0500

    Update MovieDetails.js

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,8 +1,8 @@
-import { React, useEffect, useState } from "react";
+import { React, useEffect, useState } from "react"
 import ReactPlayer from 'react-player'
-import { useParams } from "react-router-dom";
-import LikesDislikes from "./LikesDislikes";
-import Reviews from "./Reviews";
+import { useParams } from "react-router-dom"
+import LikesDislikes from "./LikesDislikes"
+import Reviews from "./Reviews"
 import { Button, Form } from 'semantic-ui-react'
 import { useAlert } from 'react-alert'
 
